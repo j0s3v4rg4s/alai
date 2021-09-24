78ba205144e042d9fec27320700e2a059d30030c
Jose Vargas <ruedagato@gmail.com>
2021-09-24 00:33:46 -0500

    fix env

--- apps/admin/src/environments/environment.prod.ts
+++ apps/admin/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true,
+    production: true,
+    fire: {
+        apiKey: 'AIzaSyD9YbJFMocl-NLTHV1UwJCUKEFgJDPFb0M',
+        authDomain: 'alai-dev-3177f.firebaseapp.com',
+        projectId: 'alai-dev-3177f',
+        storageBucket: 'alai-dev-3177f.appspot.com',
+        messagingSenderId: '942112183968',
+        appId: '1:942112183968:web:5189ec50b8d5609ef91bc5',
+        measurementId: 'G-HTM5PJJQVZ',
+    },
+    useEmulator: false,
 };
